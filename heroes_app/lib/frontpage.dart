d161aa674a6902f33b85108ab333fd577443354e
hanna <hannaevensen2405@gmail.com>
2019-02-19 13:05:21 +0100

    #1 add title

--- heroes_app/lib/frontpage.dart
+++ heroes_app/lib/frontpage.dart
@@ -22,20 +22,31 @@ class _FrontPageState extends State<FrontPage> {
             child: Image.asset('assets/logo.png'),
         ),
     );
+    
     //App name
     final appName = new Text(
-        'Heroes of the gym', style: TextStyle(fontWeight: FontWeight.bold, fontSize: 25.0,), textAlign: TextAlign.center,
+        'Heroes of the gym', style: TextStyle(color: Colors.white, fontWeight: FontWeight.bold, fontSize: 25.0,), textAlign: TextAlign.center,
     );
 
     //Login button
-    final loginButton = Padding(
-        padding: EdgeInsets.symmetric(vertical: 0.0),
+    final loginButton = new Container(
+        margin: const EdgeInsets.only(left: 30, right: 30),
         child: RaisedButton(
             elevation: 5.0,
+            color: Colors.white,
             shape: RoundedRectangleBorder(
                 borderRadius: BorderRadius.circular(15.0),
             ),
-            child: Text('Log In', style: TextStyle(color: const Color(0xFF4FB88B), fontSize: 20), ), color: Colors.white,
+            child: GestureDetector(
+                child: Center(
+                    child: Text('Log In', style: TextStyle(
+                            color: const Color(0xFF4FB88B),
+                            fontSize: 20.0,
+                        ),),
+                   // color: Colors.white,
+                )
+            ),
+            //child: Text('Log In', style: TextStyle(color: const Color(0xFF4FB88B), fontSize: 20), ), color: Colors.white,
             onPressed: () {
                 Navigator.of(context).pushNamed(LoginPage.tag);
             },
@@ -44,10 +55,11 @@ class _FrontPageState extends State<FrontPage> {
 
 
     //Signup button
-    final signupButton = Padding(
-        padding: EdgeInsets.symmetric(vertical: 0.0),
+    final signupButton = new Container(
+        margin: const EdgeInsets.only(left: 30, right: 30),
         child: RaisedButton(
             color: const Color(0xFF4FB88B),
+            elevation: 5.0,
             shape: OutlineInputBorder(
                 borderRadius: BorderRadius.circular(60),
                 borderSide: BorderSide(
@@ -90,18 +102,11 @@ class _FrontPageState extends State<FrontPage> {
                                 logo,
                             ],
                         ),
-                        SizedBox(height: 120.0),
-                        Container(
-                            height: 40.0,
-                            width: 250.0,
-                            child: loginButton,
-                        ),
+                        appName,
+                        SizedBox(height: 80.0),
+                        loginButton,
                         SizedBox(height: 20.0),
-                        Container(
-                            height: 40.0,
-                            width: 250.0,
-                            child: signupButton,
-                            )
+                        signupButton,
                         ],
                     )
                 ],
