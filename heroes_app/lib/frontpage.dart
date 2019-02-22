baaf82f404ae8607c8bbb4972331ea9c493df27d
Maria Osen <marose42@gmail.com>
2019-02-22 16:26:07 +0100

    #1 #4 Changed elements in frontpage to widgets

--- heroes_app/lib/frontpage.dart
+++ heroes_app/lib/frontpage.dart
@@ -22,50 +22,60 @@ class _FrontPageState extends State<FrontPage> {
     Widget build(BuildContext context) {
 
     //placeholder for logo
-    final logo = Hero(
+    Widget logo() {
+      return new Hero(
         tag: 'hero',
         child: CircleAvatar(
-            backgroundColor: Colors.transparent,
-            radius: 60.0,
-            child: Image.asset('assets/logo.png'),
+          backgroundColor: Colors.transparent,
+          radius: 60.0,
+          child: Image.asset('assets/logo.png'),
         ),
-    );
+      );
+    }
     
     //App name
-    final appName = new Text(
-        'Heroes of the Gym', style: TextStyle(color: Colors.white, fontWeight: FontWeight.bold, fontSize: 25.0,), textAlign: TextAlign.center,
-    );
+    Widget appName() {
+      return new Text(
+        'Heroes of the Gym', style: TextStyle(
+        color: Colors.white, fontWeight: FontWeight.bold, fontSize: 25.0,),
+        textAlign: TextAlign.center,
+      );
+    }
 
     //Login button
-    final loginButton = new Container(
+    Widget loginButton() {
+      new Container(
         margin: const EdgeInsets.only(left: 30, right: 30),
         child: RaisedButton(
-            elevation: 5.0,
-            color: Colors.white,
-            shape: RoundedRectangleBorder(
-                borderRadius: BorderRadius.circular(15.0),
-            ),
-            child: GestureDetector(
-                child: Center(
-                    child: Text('Log In', style: TextStyle(
-                            color: const Color(0xFF4FB88B),
-                            fontSize: 20.0,
-                        ),),
-                   // color: Colors.white,
-                )
-            ),
-            //child: Text('Log In', style: TextStyle(color: const Color(0xFF4FB88B), fontSize: 20), ), color: Colors.white,
-            onPressed: () {
-                widget.readyToLogIn();
-            },
+          elevation: 5.0,
+          color: Colors.white,
+          shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(60),
+          ),
+          child: GestureDetector(
+              child: Center(
+                child: Text('Log In', style: TextStyle(
+                  color: const Color(0xFF4FB88B),
+                  fontSize: 20.0,
+                ),),
+                // color: Colors.white,
+              )
+          ),
+          //child: Text('Log In', style: TextStyle(color: const Color(0xFF4FB88B), fontSize: 20), ), color: Colors.white,
+          key: Key('LogIn'),
+          onPressed: () {
+            widget.readyToLogIn();
+          },
         ),
-    );
+      );
+    }
 
 
-    //Signup button
-    final signupButton = new Container(
-        margin: const EdgeInsets.only(left: 30, right: 30),
-        child: RaisedButton(
+      //Signup button
+      Widget signupButton() {
+        return new Container(
+          margin: const EdgeInsets.only(left: 30, right: 30),
+          child: RaisedButton(
             color: const Color(0xFF4FB88B),
             elevation: 5.0,
             shape: OutlineInputBorder(
@@ -77,48 +87,50 @@ class _FrontPageState extends State<FrontPage> {
             ),
             child: GestureDetector(
                 child: Center(
-                    child: Text('Sign up',
-                        style: TextStyle(
-                            color: Colors.white,
-                            fontSize: 20.0,
-                        )
-                    ),
+                  child: Text('Sign Up',
+                      style: TextStyle(
+                        color: Colors.white,
+                        fontSize: 20.0,
+                      )
+                  ),
                 )
             ),
+            key: Key('Sign Up'),
             onPressed: () {
-                widget.readyToSignUp();
+              widget.readyToSignUp();
             },
-        ),
-    );
-
-
-        return Scaffold(
-            appBar: AppBar(
-                backgroundColor: const Color(0xFF4FB88B),
-                elevation: 0.0,
-                automaticallyImplyLeading: false,
-            ),
-            backgroundColor: const Color(0xFF4FB88B),
-            body: Stack(
-                children: <Widget>[
-                    Column(
-                    children: <Widget>[
-                        SizedBox(height: 50),
-                        Row(
-                            mainAxisAlignment: MainAxisAlignment.center,
-                            children: <Widget>[
-                                logo,
-                            ],
-                        ),
-                        appName,
-                        SizedBox(height: 80.0),
-                        loginButton,
-                        SizedBox(height: 20.0),
-                        signupButton,
-                        ],
-                    )
-                ],
-            ),
+          ),
         );
+      }
+
+
+      return Scaffold(
+        appBar: AppBar(
+          backgroundColor: const Color(0xFF4FB88B),
+          elevation: 0.0,
+          automaticallyImplyLeading: false,
+        ),
+        backgroundColor: const Color(0xFF4FB88B),
+        body: Stack(
+          children: <Widget>[
+            Column(
+              children: <Widget>[
+                SizedBox(height: 50),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: <Widget>[
+                    logo(),
+                  ],
+                ),
+                appName(),
+                SizedBox(height: 80.0),
+                loginButton(),
+                SizedBox(height: 20.0),
+                signupButton(),
+              ],
+            )
+          ],
+        ),
+      );
     }
-}
+    }
