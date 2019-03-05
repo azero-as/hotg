fe05d8fa9e91b90476d27d4160b0a0abe2439089
Hedda Nikolaisen Holmen <heddanh@stud.ntnu.no>
2019-03-05 15:10:47 +0100

    #26 get level cap

--- heroes_app/lib/levelUp.dart
+++ heroes_app/lib/levelUp.dart
@@ -10,8 +10,13 @@ class LevelUp extends StatefulWidget{
 
 class _LevelUpState extends State<LevelUp>{
 
-  int _userXP = 0;
   String _uid = "";
+  int _userLevel = 0;
+  int _userXP = 0;
+
+  var _listUser = [];
+  var _listXP = [];
+  var _listLevel = [];
 
   @override
   void initState(){
@@ -29,15 +34,17 @@ class _LevelUpState extends State<LevelUp>{
 
   @override
   Widget build(BuildContext context) {
-    _getXP();
-    print("Current user ID: ${_uid}");
-    print("XP: ${_userXP}");
+    _getUsersLevelXP();
+    var xp = _getNextLevelCap();
+    print("Cap: ${xp}");
+
     return new Scaffold(
       body: new Center(
         child: new Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
-            new Text("LevelUp"),
+            new Text("Level: ${_userLevel}"),
+            new Text("Next level cap: ${xp}"),
           ],
         ),
       ),
@@ -45,12 +52,49 @@ class _LevelUpState extends State<LevelUp>{
   }
 
 
-  void _getXP(){
-    final userDoc = Firestore.instance.document("Users/${_uid}");
+  void _getUsersLevelXP(){
 
-    print("Print: ${userDoc}");
+    //query from database
+    Firestore.instance
+        .collection('Users')
+        .where("XP", isGreaterThanOrEqualTo: 0)
+        .snapshots()
+        .listen((data) =>
+        data.documents.forEach((doc) => {
+            _listUser.add(doc.documentID),
+            _listXP.add(doc["XP"]),
+            _listLevel.add(doc["Level"]),
+    }));
 
+    //if userId == documentID, set level and XP
+    for (var i = 0; i < _listUser.length; i++){
+      if (_listUser[i].toString() == _uid){
+        _userXP = _listXP[i];
+        _userLevel = _listLevel[i];
+      }
+    }
 
+    //empty the lists
+    _listUser = [];
+    _listXP = [];
+    _listLevel = [];
+  }
+
+  _getNextLevelCap(){
+
+    var _xpCap = 0;
+
+    //query from database
+    Firestore.instance
+        .collection('Levels')
+        .where("Level", isEqualTo: _userLevel)
+        .snapshots()
+        .listen((data) =>
+        data.documents.forEach((doc) => {
+          _xpCap = doc["XP cap"],
+        print("_xpCap: ${_xpCap}"),
+        }));
+    return _xpCap;
   }
 
 }
