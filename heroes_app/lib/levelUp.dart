8e825164bab5a562f9066513b8f67376a2ccb1e4
Hedda Nikolaisen Holmen <heddanh@stud.ntnu.no>
2019-03-06 15:38:59 +0100

    #26 cloud functions

--- heroes_app/lib/levelUp.dart
+++ heroes_app/lib/levelUp.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
-import 'package:firebase_auth/firebase_auth.dart';
+import 'package:cloud_functions/cloud_functions.dart';
 
 class LevelUp extends StatefulWidget{
 
@@ -10,33 +10,47 @@ class LevelUp extends StatefulWidget{
 
 class _LevelUpState extends State<LevelUp>{
 
-  String _uid = "";
   int _userLevel = 0;
   int _userXP = 0;
-
-  var _listUser = [];
-  var _listXP = [];
-  var _listLevel = [];
+  int _levelCap = 0;
 
   @override
   void initState(){
     super.initState();
 
-    //Get the userID of current user
+    CloudFunctions.instance.call(
+      functionName: 'getUserXP',
+    ).then((response) {
+      setState(() {
+        _userXP = response['XP'];
+      });
+    }).catchError((error) {
+      print(error);
+    });
+
+    CloudFunctions.instance.call(
+      functionName: 'getUserLevel',
+    ).then((response) {
+      setState(() {
+        _userLevel = response['Level'];
+      });
+    }).catchError((error) {
+      print(error);
+    });
+
+    /*//Get the userID of current user
     FirebaseAuth.instance.currentUser().then((response){
       setState(() {
         _uid = response.uid;
       });
     }).catchError((error){
       print(error);
-    });
+    });*/
   }
 
   @override
   Widget build(BuildContext context) {
-    _getUsersLevelXP();
-    var xp = _getNextLevelCap();
-    print("Cap: ${xp}");
+    getLevelCap(_userLevel);
 
     return new Scaffold(
       body: new Center(
@@ -44,57 +58,81 @@ class _LevelUpState extends State<LevelUp>{
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
             new Text("Level: ${_userLevel}"),
-            new Text("Next level cap: ${xp}"),
+            new Text("XP: ${_userXP}"),
+            new Text("Next level cap: ${_levelCap}"),
+            levelUp(),
           ],
         ),
       ),
     );
   }
 
-
-  void _getUsersLevelXP(){
-
-    //query from database
-    Firestore.instance
-        .collection('Users')
-        .where("XP", isGreaterThanOrEqualTo: 0)
-        .snapshots()
-        .listen((data) =>
-        data.documents.forEach((doc) => {
-            _listUser.add(doc.documentID),
-            _listXP.add(doc["XP"]),
-            _listLevel.add(doc["Level"]),
-    }));
-
-    //if userId == documentID, set level and XP
-    for (var i = 0; i < _listUser.length; i++){
-      if (_listUser[i].toString() == _uid){
-        _userXP = _listXP[i];
-        _userLevel = _listLevel[i];
-      }
-    }
-
-    //empty the lists
-    _listUser = [];
-    _listXP = [];
-    _listLevel = [];
-  }
-
-  _getNextLevelCap(){
-
-    var _xpCap = 0;
-
+//get cap to next level
+  void getLevelCap(level){
     //query from database
     Firestore.instance
         .collection('Levels')
-        .where("Level", isEqualTo: _userLevel)
+        .where("Level", isEqualTo: level)
         .snapshots()
         .listen((data) =>
         data.documents.forEach((doc) => {
-          _xpCap = doc["XP cap"],
-        print("_xpCap: ${_xpCap}"),
+          _levelCap = doc["xpCap"],
+          //print("xpCap: $_levelCap"),
         }));
-    return _xpCap;
+    //print("Cap: $_levelCap");
+  }
+
+//check if xp is bigger than level cap
+  checkLevelUp(){
+    //xp >= cap -> level up
+    if (_userXP >= _levelCap){
+      return true;
+    } else {
+      return false;
+    }
+  }
+
+//set one level up in db
+  void setLevel(){
+    
+  }
+
+//reset xp in db
+  void resetXP(){
+
+  }
+
+//Level Up
+  Widget levelUp(){
+    if (checkLevelUp()){
+      setLevel();
+      resetXP();
+
+      return new Container(
+        padding: EdgeInsets.fromLTRB(0, 20, 0, 0),
+        child:
+        new RichText(
+          text:
+          TextSpan(
+            style: DefaultTextStyle.of(context).style,
+            children: <TextSpan>[
+          TextSpan(
+            text: 'Congratulations! You are now: \n',
+            style: TextStyle(color: Colors.black, fontSize: 15),
+          ),
+          TextSpan(
+            text: 'Level $_userLevel\n',
+            style: TextStyle(fontWeight: FontWeight.bold),
+          ),
+          TextSpan(
+            text: 'Novice',
+             style: TextStyle(),
+          ),],)),
+      );} else {
+      return new Container(
+        child: new Text(""),
+      );
+    }
   }
 
 }
