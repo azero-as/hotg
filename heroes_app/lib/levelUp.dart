e7c949037ae492de0cc42f7d999845315a582177
Hedda Nikolaisen Holmen <heddanh@stud.ntnu.no>
2019-03-18 13:49:36 +0100

    #26 Pop-up widget

--- heroes_app/lib/levelUp.dart
+++ heroes_app/lib/levelUp.dart
@@ -13,9 +13,6 @@ class LevelUp extends StatefulWidget{
 class _LevelUpState extends State<LevelUp>{
 
   int _userLevel;
-  int _userXp;
-  int _xpCap;
- // bool _updateLevel;
 
   @override
   void initState(){
@@ -25,25 +22,14 @@ class _LevelUpState extends State<LevelUp>{
       functionName: 'updateUserLevelInfo',
     ).then((response) {
       setState(() {
-        _userXp = response['userXp'];
+        //_userXp = response['userXp'];
         _userLevel = response['userLevel'];
-        _xpCap = response['xpCap'];
+        //_xpCap = response['xpCap'];
      //   _updateLevel = response['updateLevel'];
       });
     }).catchError((error) {
       print(error);
     });
-
-   
-
-    /*//Get the userID of current user
-    FirebaseAuth.instance.currentUser().then((response){
-      setState(() {
-        _uid = response.uid;
-      });
-    }).catchError((error){
-      print(error);
-    });*/
   }
 
   @override
@@ -56,7 +42,7 @@ class _LevelUpState extends State<LevelUp>{
           children: <Widget>[
             new RaisedButton(
               onPressed: () {
-                _levelUpAlert(context);
+                showDialog(context: context,builder: (context) => _onLevelUp(context)); // Call the Dialog.
               },
               child: const Text("Pop-up"),
             ),
@@ -69,23 +55,85 @@ class _LevelUpState extends State<LevelUp>{
   }
 
 
-  Future<void> _levelUpAlert(BuildContext context) {
-    return showDialog<void>(
-      context: context,
-      builder: (BuildContext context) {
-        return AlertDialog(
-          title: Text('Level up!'),
-          content: new Text('Congratulations! You are now: Level ${_userLevel}'),
-          actions: <Widget>[
-            FlatButton(
-              child: Text('Close'),
-              onPressed: () {
-                Navigator.of(context).pop();
-              },
-            ),
-          ],
-        );
-      },
+  //Pop-up window when a user level up
+  Widget _onLevelUp(BuildContext context) {
+    return Stack(
+      alignment: Alignment.center,
+      children: <Widget>[
+        new Container(
+          padding: EdgeInsets.fromLTRB(0, 0, 0, 0),
+          margin: EdgeInsets.fromLTRB(35, 0, 35, 35),
+          //decoration: new BoxDecoration(                  //border if we want
+            //color: Colors.white,
+            //border: new Border.all(color: Colors.black)),
+          child:
+            new ListView(
+            shrinkWrap: true,
+            children: <Widget>[
+              new Container(
+                padding: EdgeInsets.fromLTRB(30, 8, 0, 8),
+                color: const Color(0xFF212838),
+                child: new Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: <Widget>[
+                    RichText(
+                      text: TextSpan(
+                        text: 'Level up!',
+                        style: TextStyle(
+                          color: Colors.white,
+                          fontWeight: FontWeight.bold,
+                          fontSize: 20,
+                        ),
+                      ),
+                    ),
+                    Align(
+                      //alignment: Alignment.topRight,
+                      child: RaisedButton(
+                          color: Theme.of(context).primaryColor,
+                          textColor: Colors.white,
+                          onPressed: () => Navigator.pop(context),
+                          child: Icon(
+                            Icons.close,
+                            color: Colors.white,
+                            ),
+                      ),
+                    ),
+                  ],
+                )
+              ),
+              new Container(
+                padding: EdgeInsets.fromLTRB(10, 40, 10, 40),
+                color: Colors.white,
+                child: Column(
+                  children: <Widget>[
+                    RichText(
+                      text: TextSpan(
+                        text: 'Congratulations! You are now: ',
+                        style: TextStyle(
+                          color: Colors.black,
+                          fontSize: 15,
+                        ),
+                      ),
+                    ),
+                    SizedBox(height: 10),
+                    RichText(
+                      text: TextSpan(
+                        text: 'Level $_userLevel',
+                        style: TextStyle(
+                          color: Colors.black,
+                          fontWeight: FontWeight.bold,
+                          fontSize: 15,
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+              //SizedBox(height: 100.0),
+            ],
+          )),
+
+      ],
     );
   }
 
