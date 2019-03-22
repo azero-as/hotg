6b1e2dec2ad315c1f78b1a91568e5f8cc156b6e8
Astrid Vik <astridvi@stud.ntnu.no>
2019-03-22 13:49:42 +0100

    #20 add correct summary page code which uses cloud function

--- heroes_app/lib/summary.dart
+++ heroes_app/lib/summary.dart
@@ -1,35 +1,26 @@
+import 'package:cloud_functions/cloud_functions.dart';
 import 'package:flutter/material.dart';
-import 'package:cloud_firestore/cloud_firestore.dart';
-import 'main.dart';
 
 class Summary extends StatefulWidget {
-  Summary({this.workoutID});
-  final String workoutID;
-
   @override
   State createState() => new _SummaryState();
 }
 
 class _SummaryState extends State<Summary> {
-  String _workoutID = '';
+  List _workouts = [];
 
   @override
   void initState() {
     super.initState();
 
-    Firestore.instance
-        .collection("Users")
-        .document("TkDkU5X55RG9rNjSb6Fn")
-        .collection("Workouts")
-        .orderBy("date", descending: true)
-        .limit(1)
-        .getDocuments()
-        .then((data) {
+    CloudFunctions.instance
+        .call(functionName: 'getAllUserWorkouts')
+        .then((response) {
       setState(() {
-        _workoutID = data.documents[0].documentID.toString();
+        _workouts = response;
       });
     }).catchError((error) {
-      return error;
+      print(error);
     });
   }
 
@@ -38,11 +29,10 @@ class _SummaryState extends State<Summary> {
     return Scaffold(
         backgroundColor: Colors.grey[300],
         appBar: AppBar(
-          backgroundColor: const Color(0xFF612A30),
           leading: new IconButton(
               icon: Icon(Icons.close),
               onPressed: () {
-                Navigator.popUntil(context, ModalRoute.withName('dashboard'));
+                // TO-DO: Add navigation back to home page
               }),
           title: Text(
             "Summary",
@@ -54,128 +44,87 @@ class _SummaryState extends State<Summary> {
             child: Column(
           children: <Widget>[
             Container(
-              // set heigth of container to be 85% of screen height
-              height: MediaQuery.of(context).size.height * 0.80,
+              // set heigth of container to be 80% of screen height
+              height: MediaQuery.of(context).size.height * 0.85,
               padding: EdgeInsets.only(
                   right: 20.0, left: 20.0, top: 30.0, bottom: 30.0),
-              child: _buildCard(),
+              child: _buildWorkoutCard(),
             )
           ],
         )));
   }
 
-  Widget _buildListItem(BuildContext context, DocumentSnapshot document) {
+  Widget _buildExerciseListItem(BuildContext context, exercise) {
     return ListTile(
-      title: Text(document["name"], style: TextStyle(fontSize: 20)),
-      trailing: Text(document["XP"].toString() + " xp",
-          style: TextStyle(fontSize: 20)),
+      title: Text(exercise["name"], style: TextStyle(fontSize: 18)),
+      trailing: Text(exercise["xp"].toString() + " xp",
+          style: TextStyle(fontSize: 18)),
       contentPadding: EdgeInsetsDirectional.only(start: 40.0, end: 40.0),
     );
   }
 
-  Widget _buildListHeader(BuildContext context) {
+  Widget _buildListHeader(BuildContext context, workouts) {
     return Container(
         child: ListTile(
       title: Text(
-        "Workout Summary",
+        workouts[0]['workoutType'] + " Workout",
         textAlign: TextAlign.center,
         style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
       ),
     ));
   }
 
-  Widget _getScore(BuildContext context) {
-    if (_workoutID == '') {
-      return Container(height: 0.0, width: 0.0);
-    } else {
-      return FutureBuilder(
-          future: Firestore.instance
-              .collection("Users")
-              .document("TkDkU5X55RG9rNjSb6Fn")
-              .collection("Workouts")
-              .document(_workoutID)
-              .get(),
-          builder: (context, AsyncSnapshot<DocumentSnapshot> snapshot) {
-            if (!snapshot.hasData) {
-              return Container(height: 0.0, width: 0.0);
-            }
-            if (snapshot.hasData) {
-              return _buildScore(context, snapshot.data);
-            }
-          });
-    }
-  }
-
-  Widget _buildScore(BuildContext context, DocumentSnapshot snapshot) {
+  Widget _buildScore(BuildContext context, workout) {
     return Column(
       children: <Widget>[
         ListTile(
-          title: Text("Bonus ", style: TextStyle(fontSize: 22)),
-          trailing: Text(snapshot["bonus_xp"].toString() + " xp",
+          title: Text("Bonus: ", style: TextStyle(fontSize: 22)),
+          trailing: Text(workout[0]["bonus_xp"].toString() + " xp",
               style: TextStyle(fontSize: 22)),
-          contentPadding: EdgeInsetsDirectional.only(start: 50.0, end: 50.0),
+          contentPadding:
+              EdgeInsetsDirectional.only(top: 10.0, start: 70.0, end: 70.0),
         ),
         ListTile(
           title: Text("Total: ",
-              style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold)),
-          trailing: Text(snapshot["total_xp"].toString() + " xp",
-              style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold)),
-          contentPadding: EdgeInsetsDirectional.only(start: 50.0, end: 50.0),
+              style: TextStyle(fontSize: 22, fontWeight: FontWeight.bold)),
+          trailing: Text(workout[0]["total_xp"].toString() + " xp",
+              style: TextStyle(fontSize: 22, fontWeight: FontWeight.bold)),
+          contentPadding: EdgeInsetsDirectional.only(start: 70.0, end: 70.0),
         ),
       ],
     );
   }
 
-  Widget _getWorkoutInfo() {
-    if (_workoutID == '') {
+  Widget _buildExerciseList() {
+    return new Scrollbar(
+        child: ListView.builder(
+            //spacing between items
+            itemExtent: 45.0,
+            itemCount: _workouts[0]['exercises'].length,
+            itemBuilder: (context, index) {
+              return _buildExerciseListItem(
+                  context, _workouts[0]['exercises'][index]);
+            }));
+  }
+
+  Widget _buildWorkoutCard() {
+    var appScreenHeight = MediaQuery.of(context).size.height;
+    if (_workouts.length == 0) {
       return Center(
         child: CircularProgressIndicator(),
       );
     } else {
-      return StreamBuilder(
-          stream: Firestore.instance
-              .collection("Users")
-              // this reference should be replaced by state.user.id
-              .document("TkDkU5X55RG9rNjSb6Fn")
-              .collection("Workouts")
-              .document(_workoutID)
-              .collection("Exercises")
-              .snapshots(),
-          builder: (context, snapshot) {
-            if (!snapshot.hasData) {
-              return Center(child: CircularProgressIndicator());
-            }
-            if (snapshot.hasError) {
-              return Text('Error: ${snapshot.error}');
-            }
-            if (snapshot.hasData) {
-              return new ListView.builder(
-
-                  //spacing between items
-                  itemExtent: 50.0,
-                  itemCount: snapshot.data.documents.length,
-                  itemBuilder: (context, index) {
-                    return _buildListItem(
-                        context, snapshot.data.documents[index]);
-                  });
-            }
-          });
+      return Card(
+          elevation: 2.0,
+          child: Column(children: <Widget>[
+            _buildListHeader(context, _workouts),
+            Container(
+              height: appScreenHeight * 0.45,
+              child: _buildExerciseList(),
+            ),
+            Divider(color: Colors.black),
+            _buildScore(context, _workouts),
+          ]));
     }
-  }
-
-  Widget _buildCard() {
-    return Card(
-        elevation: 2.0,
-        child: Column(children: <Widget>[
-          _buildListHeader(context),
-          Container(
-            height: MediaQuery.of(context).size.height * 0.45,
-            child: _getWorkoutInfo(),
-          ),
-          Divider(color: Colors.black),
-          Container(
-              height: MediaQuery.of(context).size.height * 0.15,
-              child: _getScore(context)),
-        ]));
   }
 }
