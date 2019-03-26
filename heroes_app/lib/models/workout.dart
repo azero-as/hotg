263cbb09e215aa3d3a2246577b9e3832a78b8ba2
Maria Osen <marose42@gmail.com>
2019-03-26 13:36:19 +0100

    #76 Add state from plan to state in workout

--- heroes_app/lib/models/workout.dart
+++ heroes_app/lib/models/workout.dart
@@ -5,15 +5,12 @@ class Workout extends Model {
 
   Auth auth = new Auth();
 
+  // Belongs to Home
   String _intensity = '';
   String _workoutName = '';
   int _duration = -1;
   int _xp = -1;
   List<dynamic> _exercises = [];
-
-  List<dynamic> _selectedExercises = [];
-  int _XpEarned = 0;
-  int _BonusXP = 0;
 
   int get xp => _xp;
   String get intensity => _intensity;
@@ -21,14 +18,30 @@ class Workout extends Model {
   int get duration => _duration;
   List<dynamic> get exercises => _exercises;
 
+  // Belongs to activeWorkoutSession
+  List<dynamic> _selectedExercises = []; //Same as exercises in activeWorkoutSession.
+  int _XpEarned = 0;
+  int _BonusXP = 0;
+
   List<dynamic> get selectedExercises => _selectedExercises;
   int get XpEarned => _XpEarned;
   int get BonusXP => _BonusXP;
+
+  // Belongs to plan
+  List<dynamic> _listOfWorkouts;
+
+  List<dynamic> get listOfWorkouts => _listOfWorkouts;
+
+  void setListOfWorkouts(List<dynamic> workouts) {
+    this._listOfWorkouts = workouts;
+    notifyListeners();
+  }
 
   void setFinishedWorkout(List<dynamic> selectedExercises, int XpEarned, int BonusXP) {
     this._selectedExercises = selectedExercises;
     this._XpEarned = XpEarned;
     this._BonusXP = BonusXP;
+    notifyListeners();
   }
 
   void setXp(int xp) {
