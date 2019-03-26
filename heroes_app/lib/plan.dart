2fd0d5f09981db4f13a5c6b7879ea39ba9632c18
Hanna Evensen <hannaevensen2405@gmail.com>
2019-03-26 22:06:12 +0100

    #90 do not display the workout on the plan page, if it does not have any exercises

--- heroes_app/lib/plan.dart
+++ heroes_app/lib/plan.dart
@@ -44,156 +44,165 @@ class _PlanPageState extends State<Plan> {
     Widget build(BuildContext context) {
 
     Widget _workout(int index) {
-      return new Container(
+      //if the workout does not have a list of exercises, do not display it as an option
+      var workout = ScopedModel.of<Workout>(context);
+      if(workout.listOfWorkouts[index]["exercises"]  == null || workout.listOfWorkouts[index]["exercises"].length == 0){
+        print("null");
+        print(workout.listOfWorkouts[index]["exercises"]);
+        return Text("");
+      }
+      else {
+        return new Container(
 
-        // add border for the workout info box
-        margin: new EdgeInsets.symmetric(horizontal: 55.0, vertical: 12.0),
-        decoration: BoxDecoration(
-          border: Border.all(color: Colors.black, width: 0.25),
-          color: Color(0xFFE7E9ED),
-        ),
-        child: ScopedModelDescendant<Workout>(builder: (context, child, model) {
-          return Column(
-          // Text starts on the left, instead of centered as is the default
-          crossAxisAlignment: CrossAxisAlignment.start,
-          mainAxisAlignment: MainAxisAlignment.spaceAround,
-          children: <Widget>[
-            // container for title box
-            Container(
-              padding: EdgeInsets.all(5),
-              // border to distinguish between the two containers within the box
-              // Colour for the entire row
-              decoration: BoxDecoration(
-                border: Border.all(color: Color(0xFF212838), width: 0.15),
-                color: Color(0xFF212838),
-              ),
-              child: Row(
-                children: <Widget>[
-                  // add some space between left-side border and beginning of text
-                  Padding(
-                    padding: EdgeInsets.fromLTRB(0, 10, 0, 10),
+          // add border for the workout info box
+          margin: new EdgeInsets.symmetric(horizontal: 55.0, vertical: 12.0),
+          decoration: BoxDecoration(
+            border: Border.all(color: Colors.black, width: 0.25),
+            color: Color(0xFFE7E9ED),
+          ),
+          child: ScopedModelDescendant<Workout>(builder: (context, child, model) {
+            return Column(
+              // Text starts on the left, instead of centered as is the default
+              crossAxisAlignment: CrossAxisAlignment.start,
+              mainAxisAlignment: MainAxisAlignment.spaceAround,
+              children: <Widget>[
+                // container for title box
+                Container(
+                  padding: EdgeInsets.all(5),
+                  // border to distinguish between the two containers within the box
+                  // Colour for the entire row
+                  decoration: BoxDecoration(
+                    border: Border.all(color: Color(0xFF212838), width: 0.15),
+                    color: Color(0xFF212838),
                   ),
-                  // new container for title
-                  Align(
-                    alignment: Alignment.centerLeft,
-                    child: Container(
-                      child: Text(
-                        model.listOfWorkouts[index]["workoutName"] ?? '',
-                        style: TextStyle(color: Colors.white),
+                  child: Row(
+                    children: <Widget>[
+                      // add some space between left-side border and beginning of text
+                      Padding(
+                        padding: EdgeInsets.fromLTRB(0, 10, 0, 10),
                       ),
-                    ),
+                      // new container for title
+                      Align(
+                        alignment: Alignment.centerLeft,
+                        child: Container(
+                          child: Text(
+                            model.listOfWorkouts[index]["workoutName"] ?? '',
+                            style: TextStyle(color: Colors.white),
+                          ),
+                        ),
+                      ),
+                    ],
                   ),
-                ],
-              ),
-            ),
+                ),
 
-            // container for changing information
-            Container(
-              padding: EdgeInsets.all(5),
-              // border to distinguish between the two containers within the box
-              decoration: BoxDecoration(
-                border: Border.all(color: Colors.black, width: 0.15),
-              ),
-              child: Row(
-                children: <Widget>[
-                  // Column for information declaration
-                  Expanded(
-                    flex: 2,
-                    child: Column(
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: <Widget>[
-                        Text(
-                          'XP:',
-                          style: TextStyle(
-                              fontWeight: FontWeight.bold,
-                              color: Color(0xFF434242)),
-                        ),
-                        // add space between lines
-                        SizedBox(
-                          height: 10,
-                        ),
-                        Text(
-                          'Intensity:',
-                          style: TextStyle(
-                              fontWeight: FontWeight.bold,
-                              color: Color(0xFF434242)),
-                        ),
-                        // add space between lines
-                        SizedBox(
-                          height: 10,
-                        ),
-                        Icon(
-                          Icons.alarm,
-                          color: Color(0xFF434242),
-                        ),
-                      ],
-                    ),
+                // container for changing information
+                Container(
+                  padding: EdgeInsets.all(5),
+                  // border to distinguish between the two containers within the box
+                  decoration: BoxDecoration(
+                    border: Border.all(color: Colors.black, width: 0.15),
                   ),
-                  // Column for changing information
-                  Expanded(
-                    flex: 3,
-                    child: Column(
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        children: <Widget>[
-                          Text(
-                            model.listOfWorkouts[index]["xp"].toString() ?? '',
-                            style: TextStyle(color: Color(0xFF434242)),
-                          ),
-                          // add space between lines
-                          SizedBox(
-                            height: 10,
-                          ),
-                          Text(
-                              model.listOfWorkouts[index]["intensity"].toString() ?? '',
-                            style: TextStyle(color: Color(0xFF434242)),
-                          ),
-                          // add space between lines
-                          SizedBox(
-                            height: 18,
-                          ),
-                          Text(
-                            model.listOfWorkouts[index]["duration"].toString() + " min" ?? '',
-                            style: TextStyle(color: Color(0xFF434242)),
-                          ),
-                        ]),
-                  ),
-                  // Column for button
-                  Expanded(
-                    flex: 3,
-                    child: Column(
-                        children: <Widget>[
-                          // add space to make the button stay at the bottom of the box
-                          SizedBox(
-                            height: 50,
-                          ),
-                          RaisedButton(
-                            padding: EdgeInsets.all(10.0),
-                            onPressed: () {
-                              model.isFromHomePage = false;
-                              model.changeActiveWorkout(model.listOfWorkouts, index);
-                              widget.onStartWorkout();
-                            },
-                            elevation: 5.0,
-                            color: Color(0xFF612A30),
-                            shape: RoundedRectangleBorder(
-                              borderRadius: BorderRadius.circular(10.67),
-                            ),
-                            child: Text(
-                              'See workout',
+                  child: Row(
+                    children: <Widget>[
+                      // Column for information declaration
+                      Expanded(
+                        flex: 2,
+                        child: Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: <Widget>[
+                            Text(
+                              'XP:',
                               style: TextStyle(
-                                  color: Colors.white, fontSize: 13.0),
+                                  fontWeight: FontWeight.bold,
+                                  color: Color(0xFF434242)),
                             ),
-                          ),
-                        ],
+                            // add space between lines
+                            SizedBox(
+                              height: 10,
+                            ),
+                            Text(
+                              'Intensity:',
+                              style: TextStyle(
+                                  fontWeight: FontWeight.bold,
+                                  color: Color(0xFF434242)),
+                            ),
+                            // add space between lines
+                            SizedBox(
+                              height: 10,
+                            ),
+                            Icon(
+                              Icons.alarm,
+                              color: Color(0xFF434242),
+                            ),
+                          ],
+                        ),
                       ),
+                      // Column for changing information
+                      Expanded(
+                        flex: 3,
+                        child: Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: <Widget>[
+                              Text(
+                                model.listOfWorkouts[index]["xp"].toString() ?? '',
+                                style: TextStyle(color: Color(0xFF434242)),
+                              ),
+                              // add space between lines
+                              SizedBox(
+                                height: 10,
+                              ),
+                              Text(
+                                model.listOfWorkouts[index]["intensity"].toString() ?? '',
+                                style: TextStyle(color: Color(0xFF434242)),
+                              ),
+                              // add space between lines
+                              SizedBox(
+                                height: 18,
+                              ),
+                              Text(
+                                model.listOfWorkouts[index]["duration"].toString() + " min" ?? '',
+                                style: TextStyle(color: Color(0xFF434242)),
+                              ),
+                            ]),
+                      ),
+                      // Column for button
+                      Expanded(
+                        flex: 3,
+                        child: Column(
+                          children: <Widget>[
+                            // add space to make the button stay at the bottom of the box
+                            SizedBox(
+                              height: 50,
+                            ),
+                            RaisedButton(
+                              padding: EdgeInsets.all(10.0),
+                              onPressed: () {
+                                model.isFromHomePage = false;
+                                model.changeActiveWorkout(model.listOfWorkouts, index);
+                                widget.onStartWorkout();
+                              },
+                              elevation: 5.0,
+                              color: Color(0xFF612A30),
+                              shape: RoundedRectangleBorder(
+                                borderRadius: BorderRadius.circular(10.67),
+                              ),
+                              child: Text(
+                                'See workout',
+                                style: TextStyle(
+                                    color: Colors.white, fontSize: 13.0),
+                              ),
+                            ),
+                          ],
+                        ),
+                      ),
+                    ],
                   ),
-                ],
-              ),
-            ),
-          ],
-        );
+                ),
+              ],
+            );
           }),
-      );
+        );
+      }
     }
 
     Widget _listOfWorkouts() {
@@ -202,17 +211,21 @@ class _PlanPageState extends State<Plan> {
         return Text("");
       }
       else {
+
         return new ListView.builder(
           scrollDirection: Axis.vertical,
           shrinkWrap: true,
           itemCount: workout.listOfWorkouts.length,
           itemBuilder: (BuildContext context, int index){
+            print("HJHj");
+            print(workout.listOfWorkouts[index]["exercises"]);
             return _workout(index);
             //children: root["info"]
           },
         );
       }
     }
+
 
     return Scaffold(
       appBar: new AppBar(
