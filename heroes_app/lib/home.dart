6a6d0a5a9c38e873482943fb6b6e8527bd80cbfe
Therese Federl <theresehfederl@gmail.com>
2019-03-27 13:40:30 +0100

    Recommended workout is now based on user's rpg class. #75

--- heroes_app/lib/home.dart
+++ heroes_app/lib/home.dart
@@ -10,8 +10,14 @@ import 'package:cloud_functions/cloud_functions.dart';
 
 // build the home page and call on the stateful classes
 class Home extends StatelessWidget {
-
-  Home({this.auth, this.onSignedOut, this.onLoggedIn, this.readyToSignOut, this.onStartWorkout, this.onActiveWorkout, this.onSummary});
+  Home(
+      {this.auth,
+      this.onSignedOut,
+      this.onLoggedIn,
+      this.readyToSignOut,
+      this.onStartWorkout,
+      this.onActiveWorkout,
+      this.onSummary});
 
   final BaseAuth auth;
   final VoidCallback onSignedOut;
@@ -33,9 +39,16 @@ class Home extends StatelessWidget {
         ),
         child: Column(
           children: <Widget>[
-            AvatarOverview(auth: auth, onSignedOut: onSignedOut, onLoggedIn: onLoggedIn, readyToSignOut: readyToSignOut),
+            AvatarOverview(
+                auth: auth,
+                onSignedOut: onSignedOut,
+                onLoggedIn: onLoggedIn,
+                readyToSignOut: readyToSignOut),
             SizedBox(height: 20.0),
-            WorkoutOverview(onStartWorkout: onStartWorkout, onActiveWorkout: onActiveWorkout, onSummary: onSummary),
+            WorkoutOverview(
+                onStartWorkout: onStartWorkout,
+                onActiveWorkout: onActiveWorkout,
+                onSummary: onSummary),
           ],
         ));
   }
@@ -43,7 +56,8 @@ class Home extends StatelessWidget {
 
 // create state for appbar of home page
 class AvatarOverview extends StatefulWidget {
-  AvatarOverview({this.auth, this.onSignedOut, this.onLoggedIn, this.readyToSignOut});
+  AvatarOverview(
+      {this.auth, this.onSignedOut, this.onLoggedIn, this.readyToSignOut});
 
   final BaseAuth auth;
   final VoidCallback onSignedOut;
@@ -71,13 +85,13 @@ class _AvatarOverviewState extends State<AvatarOverview> {
               width: barWidth,
               color: Color(0xFF212838),
               padding: EdgeInsets.fromLTRB(20, 30, 20, 15),
-              child: ScopedModelDescendant<User>(builder: (context, child, model) {
-                // Percent should not exceed 1.0: 
-                double xpPercent; 
-                 if (model.xp >= model.xpCap) {
-                   xpPercent = 1.0;
-                   }
-                else {
+              child:
+                  ScopedModelDescendant<User>(builder: (context, child, model) {
+                // Percent should not exceed 1.0:
+                double xpPercent;
+                if (model.xp >= model.xpCap) {
+                  xpPercent = 1.0;
+                } else {
                   xpPercent = model.xp / model.xpCap;
                 }
                 return Row(
@@ -101,13 +115,14 @@ class _AvatarOverviewState extends State<AvatarOverview> {
                       children: <Widget>[
                         //Settings symbol and onPressed functionality
                         IconButton(
-                          icon: Icon(Icons.settings),
-                          key: Key("settingsButton"),
-                          color: Colors.white,
-                          padding: EdgeInsets.fromLTRB((barWidth / 2) - 44, 0, 0, 0),
-                          onPressed: () {
-                            widget.readyToSignOut();
-                          }),
+                            icon: Icon(Icons.settings),
+                            key: Key("settingsButton"),
+                            color: Colors.white,
+                            padding: EdgeInsets.fromLTRB(
+                                (barWidth / 2) - 44, 0, 0, 0),
+                            onPressed: () {
+                              widget.readyToSignOut();
+                            }),
 
                         //Username
                         Padding(
@@ -177,14 +192,18 @@ class WorkoutOverview extends StatefulWidget {
   final VoidCallback onStartWorkout;
   final VoidCallback onActiveWorkout;
   final VoidCallback onSummary;
-
 }
 
 // class for workout overview
 class _WorkoutOverviewState extends State<WorkoutOverview> {
   @override
+  void initState() {
+    super.initState();
+  }
+
+  @override
   Widget build(BuildContext context) {
-  var workout = ScopedModel.of<Workout>(context);
+    var workout = ScopedModel.of<Workout>(context);
     return LayoutBuilder(builder: (context, constraints) {
       if (workout.intensity == "" ||
           workout.workoutName == "" ||
@@ -209,159 +228,157 @@ class _WorkoutOverviewState extends State<WorkoutOverview> {
                   ),
                 ),
                 // call on workout widget
-                _workout(),
+                _workout(workout),
               ],
             ));
       }
     });
   }
 
-  Widget _workout() {
+  Widget _workout(workoutModel) {
     return new Container(
-      // add border for the workout info box
-      decoration: BoxDecoration(
-        border: Border.all(color: Colors.black, width: 0.25),
-        color: Color(0xFFE7E9ED),
-      ),
-      child: ScopedModelDescendant<Workout>(builder: (context, child, model) {
-      return Column(
-        // Text starts on the left, instead of centered as is the default
-        crossAxisAlignment: CrossAxisAlignment.start,
-        mainAxisAlignment: MainAxisAlignment.spaceAround,
-        children: <Widget>[
-          // container for title box
-          Container(
-            padding: EdgeInsets.all(5),
-            // border to distinguish between the two containers within the box
-            // Colour for the entire row
-            decoration: BoxDecoration(
-              border: Border.all(color: Color(0xFF212838), width: 0.15),
-              color: Color(0xFF212838),
-            ),
-            child: Row(
-              children: <Widget>[
-                // add some space between left-side border and beginning of text
-                Padding(
-                  padding: EdgeInsets.fromLTRB(0, 10, 0, 10),
-                ),
-                // new container for title
-                Align(
-                  alignment: Alignment.centerLeft,
-                  child: Container(
-                    child: Text(
-                      model.workoutName,
-                      style: TextStyle(color: Colors.white),
+        // add border for the workout info box
+        decoration: BoxDecoration(
+          border: Border.all(color: Colors.black, width: 0.25),
+          color: Color(0xFFE7E9ED),
+        ),
+        child: Column(
+          // Text starts on the left, instead of centered as is the default
+          crossAxisAlignment: CrossAxisAlignment.start,
+          mainAxisAlignment: MainAxisAlignment.spaceAround,
+          children: <Widget>[
+            // container for title box
+            Container(
+              padding: EdgeInsets.all(5),
+              // border to distinguish between the two containers within the box
+              // Colour for the entire row
+              decoration: BoxDecoration(
+                border: Border.all(color: Color(0xFF212838), width: 0.15),
+                color: Color(0xFF212838),
+              ),
+              child: Row(
+                children: <Widget>[
+                  // add some space between left-side border and beginning of text
+                  Padding(
+                    padding: EdgeInsets.fromLTRB(0, 10, 0, 10),
+                  ),
+                  // new container for title
+                  Align(
+                    alignment: Alignment.centerLeft,
+                    child: Container(
+                      child: Text(
+                        workoutModel.workoutName,
+                        style: TextStyle(color: Colors.white),
+                      ),
                     ),
                   ),
-                ),
-              ],
+                ],
+              ),
             ),
-          ),
 
-          // container for changing information
-          Container(
-            padding: EdgeInsets.all(5),
-            // border to distinguish between the two containers within the box
-            decoration: BoxDecoration(
-              border: Border.all(color: Colors.black, width: 0.15),
-            ),
-            child: Row(
-              children: <Widget>[
-                // Column for information declaration
-                Expanded(
-                  flex: 2,
-                  child: Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: <Widget>[
-                      Text(
-                        'XP:',
-                        style: TextStyle(
-                            fontWeight: FontWeight.bold,
-                            color: Color(0xFF434242)),
-                      ),
-                      // add space between lines
-                      SizedBox(
-                        height: 10,
-                      ),
-                      Text(
-                        'Intensity:',
-                        style: TextStyle(
-                            fontWeight: FontWeight.bold,
-                            color: Color(0xFF434242)),
-                      ),
-                      // add space between lines
-                      SizedBox(
-                        height: 10,
-                      ),
-                      Icon(
-                        Icons.alarm,
-                        color: Color(0xFF434242),
-                      ),
-                    ],
-                  ),
-                ),
-                // Column for changing information
-                Expanded(
-                  flex: 3,
-                  child: Column(
+            // container for changing information
+            Container(
+              padding: EdgeInsets.all(5),
+              // border to distinguish between the two containers within the box
+              decoration: BoxDecoration(
+                border: Border.all(color: Colors.black, width: 0.15),
+              ),
+              child: Row(
+                children: <Widget>[
+                  // Column for information declaration
+                  Expanded(
+                    flex: 2,
+                    child: Column(
                       crossAxisAlignment: CrossAxisAlignment.start,
                       children: <Widget>[
                         Text(
-                          model.xp.toString(),
-                          style: TextStyle(color: Color(0xFF434242)),
+                          'XP:',
+                          style: TextStyle(
+                              fontWeight: FontWeight.bold,
+                              color: Color(0xFF434242)),
                         ),
                         // add space between lines
                         SizedBox(
                           height: 10,
                         ),
                         Text(
-                          model.intensity,
-                          style: TextStyle(color: Color(0xFF434242)),
+                          'Intensity:',
+                          style: TextStyle(
+                              fontWeight: FontWeight.bold,
+                              color: Color(0xFF434242)),
                         ),
                         // add space between lines
                         SizedBox(
-                          height: 18,
+                          height: 10,
                         ),
-                        Text(
-                          model.duration.toString(),
-                          style: TextStyle(color: Color(0xFF434242)),
+                        Icon(
+                          Icons.alarm,
+                          color: Color(0xFF434242),
                         ),
-                      ]),
-                ),
-                // Column for button
-                Expanded(
-                  flex: 3,
-                  child: Column(
-                    children: <Widget>[
-                      // add space to make the button stay at the bottom of the box
-                      SizedBox(
-                        height: 50,
-                      ),
-                      RaisedButton(
-                        padding: EdgeInsets.all(10.0),
-                        onPressed: () {
-                          model.isFromHomePage = true;
-                          widget.onStartWorkout();
-                        },
-                        elevation: 5.0,
-                        color: Color(0xFF612A30),
-                        shape: RoundedRectangleBorder(
-                          borderRadius: BorderRadius.circular(10.67),
-                        ),
-                        child: Text(
-                          'See workout',
-                          style: TextStyle(color: Colors.white, fontSize: 13.0),
-                        ),
-                      ),
-                    ],
+                      ],
+                    ),
                   ),
-                ),
-              ],
+                  // Column for changing information
+                  Expanded(
+                    flex: 3,
+                    child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: <Widget>[
+                          Text(
+                            workoutModel.xp.toString(),
+                            style: TextStyle(color: Color(0xFF434242)),
+                          ),
+                          // add space between lines
+                          SizedBox(
+                            height: 10,
+                          ),
+                          Text(
+                            workoutModel.intensity,
+                            style: TextStyle(color: Color(0xFF434242)),
+                          ),
+                          // add space between lines
+                          SizedBox(
+                            height: 18,
+                          ),
+                          Text(
+                            workoutModel.duration.toString(),
+                            style: TextStyle(color: Color(0xFF434242)),
+                          ),
+                        ]),
+                  ),
+                  // Column for button
+                  Expanded(
+                    flex: 3,
+                    child: Column(
+                      children: <Widget>[
+                        // add space to make the button stay at the bottom of the box
+                        SizedBox(
+                          height: 50,
+                        ),
+                        RaisedButton(
+                          padding: EdgeInsets.all(10.0),
+                          onPressed: () {
+                            workoutModel.isFromHomePage = true;
+                            widget.onStartWorkout();
+                          },
+                          elevation: 5.0,
+                          color: Color(0xFF612A30),
+                          shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(10.67),
+                          ),
+                          child: Text(
+                            'See workout',
+                            style:
+                                TextStyle(color: Colors.white, fontSize: 13.0),
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                ],
+              ),
             ),
-          ),
-        ],
-      );
-      })
-    );
+          ],
+        ));
   }
 }
