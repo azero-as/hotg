2fbac1ebbab84174fdf1334f42c9589dcdd10be7
Marianne Voss Siljan <marianvs@stud.ntnu.no>
2019-03-29 15:12:53 +0100

    #136 Fixes keyboard overlap for signup page on Android

--- heroes_app/lib/signup.dart
+++ heroes_app/lib/signup.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'authentication.dart';
+import 'ensureVisibleWhenFocused.dart';
 
 //This is the signup page
 
@@ -28,6 +29,10 @@ enum FormMode { SIGNUP }
 class _SignupPageState extends State<SignupPage> {
   final _formKey = new GlobalKey<FormState>();
 
+  FocusNode _focusNodeEmail = new FocusNode();
+  FocusNode _focusNodePassword = new FocusNode();
+  FocusNode _focusNodePassword2 = new FocusNode();
+
   FormMode _formMode = FormMode.SIGNUP;
 
   String _email;
@@ -36,11 +41,13 @@ class _SignupPageState extends State<SignupPage> {
   String _errorMessage;
 
   bool _isIos;
+  bool _isAndroid;
   bool _isLoading;
 
   @override
   Widget build(BuildContext context) {
     _isIos = Theme.of(context).platform == TargetPlatform.iOS;
+    _isAndroid = Theme.of(context).platform == TargetPlatform.android;
 
     //Returns all the elements to the page
     return new Theme(
@@ -68,27 +75,51 @@ class _SignupPageState extends State<SignupPage> {
   }
 
   // ------ FORM WIDGETS ------
-  Widget _returnForm() {
-    return new Container(
-        padding: EdgeInsets.only(left: 24.0, right: 24.0),
-        margin: EdgeInsets.fromLTRB(35, 0, 35, 35),
-        child: new Form(
-          key: _formKey,
-          child: new ListView(
-            shrinkWrap: true,
-            children: <Widget>[
-              SizedBox(height: 25.0),
-              _logo(),
-              _welcomeText('First, we need the basics'),
-              _emailInput(),
-              _passwordInput1(),
-              _passwordInput2(),
-              _nextButton(),
-              _loginButton(),
-              _showErrorMessage(),
-            ],
-          ),
-        ));
+  _returnForm() {
+    if (_isIos){
+      return new Container(
+          padding: EdgeInsets.only(left: 24.0, right: 24.0),
+          margin: EdgeInsets.fromLTRB(35, 0, 35, 35),
+          child: new Form(
+            key: _formKey,
+            child: new ListView(
+              shrinkWrap: true,
+              children: <Widget>[
+                SizedBox(height: 25.0),
+                _logo(),
+                _welcomeText('First, we need the basics'),
+                _emailInput(),
+                _passwordInput1(),
+                _passwordInput2(),
+                _nextButton(),
+                _loginButton(),
+                _showErrorMessage(),
+              ],
+            ),
+          ));
+    } else if (_isAndroid){
+      return new Container(
+          padding: EdgeInsets.only(left: 24.0, right: 24.0),
+          margin: EdgeInsets.fromLTRB(35, 0, 35, 35),
+          child: new Form(
+            key: _formKey,
+            child: new ListView(
+              shrinkWrap: true,
+              children: <Widget>[
+                SizedBox(height: 25.0),
+                _logo(),
+                _welcomeText('First, we need the basics'),
+                _emailInput(),
+                _passwordInput1(),
+                _passwordInput2(),
+                _nextButton(),
+                _loginButton(),
+                _showErrorMessage(),
+                SizedBox(height: 250),
+              ],
+            ),
+          ));
+    }
   }
 
   //placeholder for logo
@@ -119,65 +150,74 @@ class _SignupPageState extends State<SignupPage> {
   }
 
   Widget _emailInput() {
-    return Padding(
-      padding: EdgeInsets.fromLTRB(0.0, 30.0, 0.0, 0.0),
-      child: TextFormField(
-        maxLines: 1,
-        keyboardType: TextInputType.emailAddress,
-        autofocus: false,
-        key: Key("signupEmail"),
-        decoration: InputDecoration(
-          icon: Icon(Icons.email),
-          labelText: 'Email',
-          contentPadding: EdgeInsets.fromLTRB(20.0, 10.0, 20.0, 10.0),
-          border: UnderlineInputBorder(),
-        ),
-        validator: (value) => value.isEmpty ? 'Email can\'t be empty' : null,
-        onSaved: (value) => _email = value,
-      ),
+    return EnsureVisibleWhenFocused(
+      focusNode: _focusNodeEmail,
+        child: Padding(
+          padding: EdgeInsets.fromLTRB(0.0, 30.0, 0.0, 0.0),
+          child: TextFormField(
+            maxLines: 1,
+            keyboardType: TextInputType.emailAddress,
+            autofocus: false,
+            key: Key("signupEmail"),
+            decoration: InputDecoration(
+              icon: Icon(Icons.email),
+              labelText: 'Email',
+              contentPadding: EdgeInsets.fromLTRB(20.0, 10.0, 20.0, 10.0),
+              border: UnderlineInputBorder(),
+            ),
+            validator: (value) => value.isEmpty ? 'Email can\'t be empty' : null,
+            onSaved: (value) => _email = value,
+          ),
+        )
     );
   }
 
   //Password input field
   Widget _passwordInput1() {
-    return Padding(
-      padding: EdgeInsets.fromLTRB(0.0, 15.00, 0.0, 0.0),
-      child: TextFormField(
-        autofocus: false,
-        key: Key("signupPassword"),
-        maxLines: 1,
-        obscureText: true,
-        decoration: InputDecoration(
-          icon: Icon(Icons.lock),
-          labelText: 'Password',
-          contentPadding: EdgeInsets.fromLTRB(20.0, 10.0, 20.0, 10.0),
-          border: UnderlineInputBorder(),
-        ),
-        validator: (value) => value.isEmpty ? 'Password can\'t be empty' : null,
-        onSaved: (value) => _password = value,
-      ),
+    return EnsureVisibleWhenFocused(
+      focusNode: _focusNodePassword,
+        child: Padding(
+          padding: EdgeInsets.fromLTRB(0.0, 15.00, 0.0, 0.0),
+          child: TextFormField(
+            autofocus: false,
+            key: Key("signupPassword"),
+            maxLines: 1,
+            obscureText: true,
+            decoration: InputDecoration(
+              icon: Icon(Icons.lock),
+              labelText: 'Password',
+              contentPadding: EdgeInsets.fromLTRB(20.0, 10.0, 20.0, 10.0),
+              border: UnderlineInputBorder(),
+            ),
+            validator: (value) => value.isEmpty ? 'Password can\'t be empty' : null,
+            onSaved: (value) => _password = value,
+          ),
+        )
     );
   }
 
   //Password confirmation input field
   Widget _passwordInput2() {
-    return Padding(
-      padding: EdgeInsets.fromLTRB(0.0, 15.00, 0.0, 0.0),
-      child: TextFormField(
-        autofocus: false,
-        key: Key("signupPassword2"),
-        maxLines: 1,
-        obscureText: true,
-        decoration: InputDecoration(
-          icon: Icon(Icons.lock),
-          labelText: 'Verify password',
-          contentPadding: EdgeInsets.fromLTRB(20.0, 10.0, 20.0, 10.0),
-          border: UnderlineInputBorder(),
-        ),
-        validator: (value) =>
+    return EnsureVisibleWhenFocused(
+      focusNode: _focusNodePassword2,
+        child: Padding(
+          padding: EdgeInsets.fromLTRB(0.0, 15.00, 0.0, 0.0),
+          child: TextFormField(
+            autofocus: false,
+            key: Key("signupPassword2"),
+            maxLines: 1,
+            obscureText: true,
+            decoration: InputDecoration(
+              icon: Icon(Icons.lock),
+              labelText: 'Verify password',
+              contentPadding: EdgeInsets.fromLTRB(20.0, 10.0, 20.0, 10.0),
+              border: UnderlineInputBorder(),
+            ),
+            validator: (value) =>
             value.isEmpty ? 'Password needs to be verified' : null,
-        onSaved: (value) => _passwordVerification = value,
-      ),
+            onSaved: (value) => _passwordVerification = value,
+          ),
+        )
     );
   }
 
