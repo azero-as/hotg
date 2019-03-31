29ce8724420899544028aad50958505428824fa4
Marianne Voss Siljan <marianvs@stud.ntnu.no>
2019-03-31 20:41:55 +0200

    Resolves #156. Info button only shown when exercise has description in database

--- heroes_app/lib/startWorkout.dart
+++ heroes_app/lib/startWorkout.dart
@@ -4,7 +4,6 @@ import 'package:scoped_model/scoped_model.dart';
 import 'models/workout.dart';
 import 'models/user.dart';
 
-
 class StartWorkout extends StatefulWidget {
   final List exercises;
   final int duration;
@@ -19,7 +18,6 @@ class StartWorkout extends StatefulWidget {
   final VoidCallback onSummary;
   final VoidCallback onBackToWorkout;
   final VoidCallback alreadyLoggedIn;
-
 
   StartWorkout(
       {this.exercises,
@@ -58,7 +56,7 @@ class _StartWorkoutPage extends State<StartWorkout> {
             widget.onActiveWorkout();
           },
           padding: EdgeInsets.fromLTRB(40, 0, 40, 0),
-          color:Theme.of(context).primaryColor,
+          color: Theme.of(context).primaryColor,
           child: Text(
             'Start workout',
             style: TextStyle(color: Colors.white),
@@ -70,65 +68,99 @@ class _StartWorkoutPage extends State<StartWorkout> {
     //General information about the workout
     Widget _showInfo() {
       return Container(
-          margin: EdgeInsets.fromLTRB(100, 0, 100, 40),
-          padding: EdgeInsets.fromLTRB(20, 20, 20, 20),
-          child: RichText(
-              text: TextSpan(
-                  style: TextStyle(
-                    fontSize: 14.0,
-                    color: Colors.black,
-                  ),
-                  children: <TextSpan>[
-
-                TextSpan(
-                    text: ' Class: ',
-                    style: TextStyle(fontWeight: FontWeight.bold)),
-                TextSpan(text: widget.workoutClass.toString() + ' \n\n'),
-                TextSpan(
-                    text: ' XP: ',
-                    style: TextStyle(fontWeight: FontWeight.bold)),
-                TextSpan(text: widget.xp.toString() + ' \n\n'),
-                TextSpan(
-                    text: ' Intensity: ',
-                    style: TextStyle(fontWeight: FontWeight.bold)),
-                TextSpan(text: widget.intensity.toString() + ' \n\n'),
-                TextSpan(
-                    text: ' Time: ',
-                    style: TextStyle(fontWeight: FontWeight.bold)),
-                TextSpan(text: widget.duration.toString() + " min"),
-              ])));
+        margin: EdgeInsets.fromLTRB(100, 0, 100, 40),
+        padding: EdgeInsets.fromLTRB(20, 20, 20, 20),
+        child: RichText(
+          text: TextSpan(
+            style: TextStyle(
+              fontSize: 14.0,
+              color: Colors.black,
+            ),
+            children: <TextSpan>[
+              TextSpan(
+                text: ' Class: ',
+                style: TextStyle(fontWeight: FontWeight.bold)),
+              TextSpan(text: widget.workoutClass.toString() + ' \n\n'),
+              TextSpan(
+                text: ' XP: ',
+                style: TextStyle(fontWeight: FontWeight.bold)),
+              TextSpan(text: widget.xp.toString() + ' \n\n'),
+              TextSpan(
+                text: ' Intensity: ',
+                style: TextStyle(fontWeight: FontWeight.bold)),
+              TextSpan(text: widget.intensity.toString() + ' \n\n'),
+              TextSpan(
+                text: ' Time: ',
+                style: TextStyle(fontWeight: FontWeight.bold)),
+              TextSpan(text: widget.duration.toString() + " min"),
+            ])));
     }
 
-    Widget _showInfoWarmUp(){
-      var workout = ScopedModel.of<Workout>(context);
-      return ExpansionTile(
-          leading: IconButton(
+    // Alert dialog to exercise description, only shown if description is in database
+    Widget _showExerciseDescription(int index) {
+      if ((widget.exercises[index]["description"]) != null) {
+        return IconButton(
           icon: Icon(Icons.info_outline),
           onPressed: () {
-              showDialog(
-                  context: context,
-                      builder: (BuildContext context) {
-                          return AlertDialog(
-                              title: Text("Warm-up"),
-                              content: Text( workout.warmUp["description"].toString()),
-                              actions: <Widget>[
-                              FlatButton(
-                              child: Text('Close'),
-                              onPressed: () {
-                                  Navigator.of(context).pop();
-                              })
-                      ],
-                  );
+            showDialog(
+              context: context,
+              builder: (BuildContext context) {
+                return AlertDialog(
+                  title: Text(widget.exercises[index]["name"]),
+                  content: Text(widget.exercises[index]["description"]),
+                  actions: <Widget>[
+                    FlatButton(
+                      child: Text('Close'),
+                      onPressed: () {
+                        Navigator.of(context).pop();
+                      })
+                  ],
+                );
               });
-          }, // title: new Text("Warm-up",
-      ),
-      title: new Text("Warm-up",),
+          },
+        );
+      } else {
+        return IconButton(
+          // TODO: if we change the background of this page, then change the color of this icon to the same color to keep in transparent
+          icon: Icon(Icons.info_outline,
+            color: Color(0xFFe0e4eb),),
+        );
+      }
+    }
+
+    Widget _showInfoWarmUp() {
+      var workout = ScopedModel.of<Workout>(context);
+      return ExpansionTile(
+        leading: IconButton(
+          icon: Icon(Icons.info_outline),
+          onPressed: () {
+            showDialog(
+              context: context,
+              builder: (BuildContext context) {
+                return AlertDialog(
+                  title: Text("Warm-up"),
+                  content: Text(workout.warmUp["description"].toString()),
+                  actions: <Widget>[
+                    FlatButton(
+                      child: Text('Close'),
+                      onPressed: () {
+                        Navigator.of(context).pop();
+                      })
+                  ],
+                );
+              });
+            }, // title: new Text("Warm-up",
+          ),
+          title: new Text(
+            "Warm-up",
+          ),
           children: <Widget>[
             ListTile(
-                title: new Text(
-                    "Minutes: " + workout.warmUp["targetMin"].toString())),
-        //children: root["info"]
-      ]);
+              title: new Text("Minutes: "
+                + workout.warmUp["targetMin"].toString())),
+            //children: root["info"]
+          ]
+      );
     }
 
     //Show information about a workout using minutes
@@ -141,56 +173,23 @@ class _StartWorkoutPage extends State<StartWorkout> {
       }
 
       return ExpansionTile(
-          leading: IconButton(
-            icon: Icon(Icons.info_outline),
-            onPressed: () {
-              showDialog(
-                  context: context,
-                  builder: (BuildContext context) {
-                    if ((widget.exercises[index]["description"]) != null){
-                      return AlertDialog(
-                        title: Text(widget.exercises[index]["name"]),
-                        content: Text(widget.exercises[index]["description"]),
-                        actions: <Widget>[
-                          FlatButton(
-                              child: Text('Close'),
-                              onPressed: () {
-                                Navigator.of(context).pop();
-                              })
-                        ],
-                      );
-                    } else {
-                      return AlertDialog(
-                        title: Text(widget.exercises[index]["name"]),
-                        content: Text('No description given unfortunately.'),
-                        actions: <Widget>[
-                          FlatButton(
-                              child: Text('Close'),
-                              onPressed: () {
-                                Navigator.of(context).pop();
-                              })
-                        ],
-                      );
-                    }
-                  });
-            },
-          ),
-          title: new Text(
+        leading: _showExerciseDescription(index),
+        title: new Text(
             (widget.exercises[index]["name"]),
           ),
           children: <Widget>[
             ListTile(
-                title: new Text("Sets: " +
-                    widget.exercises[index]["targetSets"].toString())),
+              title: new Text("Sets: " +
+                widget.exercises[index]["targetSets"].toString())),
             ListTile(
-                title: new Text(
-                    name + widget.exercises[index][exercise].toString())),
+              title: new Text(
+                name + widget.exercises[index][exercise].toString())),
             ListTile(
-                title: new Text("Rest between sets: " +
-                    widget.exercises[index]["restBetweenSets"].toString())),
+              title: new Text("Rest between sets: " +
+                widget.exercises[index]["restBetweenSets"].toString())),
             ListTile(
-                title: new Text(
-                    "XP: " + widget.exercises[index]["xp"].toString())),
+              title: new Text(
+                "XP: " + widget.exercises[index]["xp"].toString())),
           ]
           //children: root["info"]
           );
@@ -203,41 +202,37 @@ class _StartWorkoutPage extends State<StartWorkout> {
         scrollDirection: Axis.vertical,
         shrinkWrap: true,
         itemCount: widget.exercises.length + 1 ?? 0,
-        itemBuilder: (BuildContext context, int index){
-            if(index == 0){
-              return _showInfoWarmUp();
-            }
-            else{
-              index = index -1;
-              return _showInfoExercises(index);
-            }
-
-        }
-      );
-
+        itemBuilder: (BuildContext context, int index) {
+          if (index == 0) {
+            return _showInfoWarmUp();
+          } else {
+            index = index - 1;
+            return _showInfoExercises(index);
+          }
+        });
     }
 
     Widget _returnBody() {
       return new Container(
-          color: Color(0xFFe0e4eb),
-          padding: EdgeInsets.only(left: 24.0, bottom: 25.0, top: 25.0, right: 24.0),
-          child: Column(
-            children: <Widget>[
-              Container(
-                child: _showInfo(),
-              ),
-              Expanded(
-                child: _showInformationWorkout(),
-              ),
-              _returnStartWorkoutButton(),
-            ],
-          ));
+        color: Color(0xFFe0e4eb),
+        padding:
+        EdgeInsets.only(left: 24.0, bottom: 25.0, top: 25.0, right: 24.0),
+        child: Column(
+          children: <Widget>[
+            Container(
+              child: _showInfo(),
+            ),
+            Expanded(
+              child: _showInformationWorkout(),
+            ),
+            _returnStartWorkoutButton(),
+          ],
+        ));
     }
 
     var workout = ScopedModel.of<Workout>(context);
     var user = ScopedModel.of<User>(context);
     return Scaffold(
-
       appBar: AppBar(
         title: new Text(workout.workoutName),
         leading: IconButton(
@@ -245,11 +240,9 @@ class _StartWorkoutPage extends State<StartWorkout> {
           onPressed: () {
             if (workout.isFromHomePage == true) {
               widget.alreadyLoggedIn();
-
-            }
-            else {
+            } else {
               widget.onBackToWorkout();
-          }
+            }
           },
           color: Colors.white,
         ),
